c19f185e8d60a58d77329d1294da784954b01cce
habibrhamatullha-dot <habibrhamatullha@gmail.com>
2026-06-13 00:07:03 +0600

    Add files via upload

--- Start.java
+++ Start.java
@@ -1,54 +1,34 @@
 public class Start {
     public static void main(String[] args) {
 
-        // Coordinate objects
-        Coordinate c1 = new Coordinate();
-        c1.setLongitude(90.41);
-        c1.setLatitude(23.81);
+        Rectangle r = new Rectangle();
+        r.setLength(10);
+        r.setWidth(5);
+        System.out.println("Rectangle Length: " + r.getLength());
+        System.out.println("Rectangle Width: " + r.getWidth());
+        System.out.println("Rectangle Area: " + r.getArea());
 
-        Coordinate c2 = new Coordinate();
-        c2.setLongitude(88.36);
-        c2.setLatitude(22.57);
+        System.out.println();
 
-        // Schedule objects
-        Schedule s1 = new Schedule();
-        s1.setHour(10);
-        s1.setMinute(30);
+        Triangle t = new Triangle();
+        t.setBase(6);
+        t.setHeight(4);
+        System.out.println("Triangle Base: " + t.getBase());
+        System.out.println("Triangle Height: " + t.getHeight());
+        System.out.println("Triangle Area: " + t.getArea());
 
-        Schedule s2 = new Schedule();
-        s2.setHour(12);
-        s2.setMinute(15);
+        System.out.println();
 
-        // Airport objects
-        Airport a1 = new Airport();
-        a1.setName("Dhaka Airport");
-        a1.setLocation(c1);
+        Circle c = new Circle();
+        c.setRadius(7);
+        System.out.println("Circle Radius: " + c.getRadius());
+        System.out.println("Circle Area: " + c.getArea());
 
-        Airport a2 = new Airport();
-        a2.setName("Kolkata Airport");
-        a2.setLocation(c2);
+        System.out.println();
 
-        // Flight object
-        Flight f = new Flight();
-        f.setFlightId("BG101");
-        f.setCapacity(180);
-        f.setDepartureFrom(a1);
-        f.setDepartureTime(s1);
-        f.setFlyingTo(a2);
-        f.setArrivalTime(s2);
-
-        // Display information
-        System.out.println("Flight ID: " + f.getFlightId());
-        System.out.println("Capacity: " + f.getCapacity());
-
-        System.out.println("Departure From: " + f.getDepartureFrom().getName());
-        System.out.println("Departure Time: " +
-                f.getDepartureTime().getHour() + ":" +
-                f.getDepartureTime().getMinute());
-
-        System.out.println("Flying To: " + f.getFlyingTo().getName());
-        System.out.println("Arrival Time: " +
-                f.getArrivalTime().getHour() + ":" +
-                f.getArrivalTime().getMinute());
+        Square s = new Square();
+        s.setSide(8);
+        System.out.println("Square Side: " + s.getSide());
+        System.out.println("Square Area: " + s.getArea());
     }
 }
